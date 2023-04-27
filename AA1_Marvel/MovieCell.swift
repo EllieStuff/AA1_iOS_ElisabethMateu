101b96a87ec1f995ccd083866803750365f27df3
Eli <45624190+EllieStuff@users.noreply.github.com>
2023-04-27 19:51:29 +0200

    table puesta ya con llamadas

---
+++ AA1_Marvel/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  AA1_Marvel
+//
+//  Created by elisabeth.mateu@enti.cat on 27/4/23.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var mImage: MyImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var mDescription: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
